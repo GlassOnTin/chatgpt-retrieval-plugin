2806d120a4b50e72b53f2f19e258d74a94238fc4
INTERNAL\SOGIWZ <ianross.williams@lr.org>
2023-06-16 11:43:20 +0100

    ...

--- models/api.py
+++ models/api.py
@@ -36,14 +36,10 @@ class DeleteResponse(BaseModel):
 class AddReferenceRequest(BaseModel):
     from_id: str
     to_id: str
-    from_property_name: str
-    to_property_name: str
 
 class DeleteReferenceRequest(BaseModel):
     from_id: str
     to_id: str
-    from_property_name: str
-    to_property_name: str
 
 class ReferenceResponse(BaseModel):
     success: bool
