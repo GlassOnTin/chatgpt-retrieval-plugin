9b228b60b14bb5661b1f172dbe3c3bcf96dde339
INTERNAL\SOGIWZ <ianross.williams@lr.org>
2023-06-16 15:15:47 +0100

    Check for empty text in openai.get_embeddings

--- services/openai.py
+++ services/openai.py
@@ -19,6 +19,9 @@ def get_embeddings(texts: List[str]) -> List[List[float]]:
     Raises:
         Exception: If the OpenAI API call fails.
     """
+    if not texts or len(texts)==0:
+        return []
+    
     # Call the OpenAI API to get the embeddings
     response = openai.Embedding.create(input=texts, model="text-embedding-ada-002")
 
